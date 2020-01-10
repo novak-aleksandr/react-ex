d6d470a91f00b6421d8c92cc996c0f51155d7e9f
Novak Aleksandr <aleks@metahouse.ru>
2020-01-10 15:00:13 +0200

    update

---
+++ src/cars/Cars.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Cars.css';
+
+class Car extends React.Component{
+    render(){
+        return (
+            <car>
+                
+            </car>
+        )
+    }
+}
+class Cars extends React.Component{
+  render(){
+      return (<main><h1>Список машин</h1>
+        <div></div></main>);
+  }
+}
+
+export default Cars;
